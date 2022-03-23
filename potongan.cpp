ba8a480bdfe9fccbaee1fa8b5122a2fc673cdbc2
Galangpd <galangpakusadewa007@gmail.com>
2022-03-23 21:56:51 +0700

    potongan 2

--- potongan.cpp
+++ potongan.cpp
@@ -26,6 +26,23 @@ int main(){
 				z+=d[i];
 			}
 			cout<<"\nTotal Keseluruhan Biaya: "<<z<<endl;
+				if(z>45000){
+				cout<<"Mendapat Diskon 10%.\n";
+				diskon=z*10/100;
+				hasil=z-diskon;
+				cout<<"Jadi Anda Harus membayar "<<hasil<<endl;
+				cout<<"Bayar= ";cin>>bayar;
+				kembalian=bayar-hasil;
+				cout<<"Kembalian= "<<kembalian<<endl;
+			}
+			else{
+				cout<<"Anda tidak mendapat diskon.\n";
+				cout<<"Bayar= ";cin>>bayar;
+				kembalian=bayar-z;
+				cout<<"Kembalian= "<<kembalian<<endl;
+			}
+			cout<<"Keluar";
+			break;
 		}
 	}
 }
